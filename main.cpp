11120458bfa61eec28d9c2c1e7beaf82185e9c2a
Rishabh <61441648+rishabhroyy@users.noreply.github.com>
2025-10-09 16:23:25 -0700

    add cockpit and brake checks

--- main.cpp
+++ main.cpp
@@ -3,24 +3,32 @@
 
 AnalogIn apps_0{PA_4};
 AnalogIn apps_1{PA_5};
+AnalogIn brake_apps{PA_6};
+DigitalIn cockpit_switch{PA_7};
 Timer timer;
 
 bool sensors_diff_threshold_crossed;
-bool program_running = true;
+bool brake_passed = false;
 
 // LUT (must have 0.0 and 1.0 x vals)
-const double LUT[5][2] = {
-    {0.0, 0.0},
-    {0.1, 0.3},
-    {0.5, 0.7},
-    {0.9, 0.9},
-    {1.0, 0.95}};
+// const double LUT[5][2] = {
+//     {0.0, 0.0},
+//     {0.1, 0.3},
+//     {0.5, 0.7},
+//     {0.9, 0.9},
+//     {1.0, 0.95}};
+
+// 1:1 LUT
+const double LUT[2][2] = {
+   {0.0, 0.0},
+   {1.0, 1.0}
+};
 
 // get percentage throttle from input percentage throttle (input being the linear %)
 double getThrottleMapping(double x)
 {
    int lut_0, lut_1;
-   for (int i = 0; i < 5; i++)
+   for (int i = 0; i < sizeof(LUT)/sizeof(LUT[0]); i++)
    {
       if (x == LUT[i][0])
       {
@@ -38,20 +46,20 @@ double getThrottleMapping(double x)
 
 const double pedal_intercept_1 = -0.25;
 const double pedal_intercept_2 = -0.3;
+const double brake_intercept = -0.25;
 const double pedal_scale_1 = 0.5;
 const double pedal_scale_2 = (0.5 / 1.2);
+const double brake_scale = 0.5;
 
 int main()
 {
    while (true)
    {
-      if (program_running)
+      if (brake_passed && cockpit_switch)
       {
-         // printf("Hello World!\n");
-
-         // pedal position in percentage from each
-         double pedal_pos_1 = pedal_scale_1 * (apps_0.read() - pedal_intercept_1);
-         double pedal_pos_2 = pedal_scale_2 * (apps_1.read() - pedal_intercept_2);
+         // Get them as percentage
+         double pedal_pos_1 = pedal_scale_1 * ((apps_0.read()*3.3) + pedal_intercept_1);
+         double pedal_pos_2 = pedal_scale_2 * ((apps_1.read()*3.3) + pedal_intercept_2);
 
          // average them
          double avg_pos = (pedal_pos_1 + pedal_pos_2) / 2;
@@ -68,7 +76,6 @@ int main()
          else if (sensors_diff_threshold_crossed && (timer.elapsed_time().count() / 1000) >= 100)
          {
             printf("0\n");
-            program_running = false;
          }
          // If timer is running and we stopped traveling, then stop and reset the timer
          else if (!sensors_diff_threshold_crossed && (timer.elapsed_time().count()) > 0)
@@ -78,10 +85,17 @@ int main()
          }
 
          // Print pedal position
-         // printf("%f%%\n", avg_pos);
+         if (!sensors_diff_threshold_crossed) {
+            printf("%f%%\n", getThrottleMapping(avg_pos)*100);
+         }
+      } else {
+         double brake_pos = brake_scale * ((brake_apps.read()*3.3) + brake_intercept);
 
-         // Print pedal position (with LUT)
-         printf("%f%%\n", getThrottleMapping(avg_pos));
+         if (brake_pos > 0.5) {
+            brake_passed = true;
+         }
+
+         printf("0\n");
       }
    }
 
